144805a20f4bc28ee5a6aba60afb548175d7a8ed
Florent <volax6600mailpro@gmail.com>
2023-06-09 09:57:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+import button
 
 # Initialisation de pygame
 pygame.init()
-screen = pygame.display.set_mode((673, 600))
-pygame.display.set_caption("Puissance 4")
+WIDTH = 673
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Puissance 4 - Tour au Rouge")
 grille = pygame.image.load("grille.png")
 clock = pygame.time.Clock()
 running = True
@@ -24,6 +27,12 @@ ROUGE = (231, 76, 60)
 JAUNE = (241, 196, 15)
 play = ROUGE
 game = True
+pause = False
+
+resume_button = button.Button(WIDTH // 2 - 60, 150, pygame.image.load("assets/button_resume.png").convert_alpha(), 1.5)
+options_button = button.Button(WIDTH // 2 - 60, 230, pygame.image.load("assets/button_options.png").convert_alpha(), 1.5)
+quit_button = button.Button(WIDTH // 2 - 60, 310, pygame.image.load("assets/button_quit.png").convert_alpha(), 1.5)
+options_menu = "main"
 
 # FONCTIONS
 
@@ -36,8 +45,10 @@ def change_couleur():
     global play
     if play == ROUGE:
         play = JAUNE
+        pygame.display.set_caption('Puissance 4 - Tour au Jaune')
     elif play == JAUNE:
         play = ROUGE
+        pygame.display.set_caption('Puissance 4 - Tour au Rouge')
 
 def get_color(position):
     couleur = pygame.Surface.get_at(screen, position)
@@ -92,7 +103,6 @@ def check_win():
                 draw = False
     if draw:
         return (True, 'NUL')
-
     return (False, 'VIDE')
 
 # BOUCLE PRINCIPALE
@@ -100,54 +110,109 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if(event.key == pygame.K_ESCAPE) and game:
+                game = False
+                pause = True
+                pygame.display.set_caption('Puissance 4 - Pause')
+
+            elif(event.key == pygame.K_ESCAPE) and not game:
+                game = True
+                pause = False
+                if play == ROUGE:
+                    pygame.display.set_caption('Puissance 4 - Tour au Rouge')
+                elif play == JAUNE:
+                    pygame.display.set_caption('Puissance 4 - Tour au Jaune')
+            elif(event.key == pygame.K_r):
+                pions = []
+                play = ROUGE
+                game = True
+                pygame.display.set_caption('Puissance 4 - Tour au Rouge')
         if event.type == pygame.KEYDOWN and game:
+            colonne = -1
+            position = False
             if(event.key == pygame.K_1 or event.key == pygame.K_KP1):
-                position = get_lower_position(0)
-                if position:
-                    afficher_pion(play, 0, position)
-                    change_couleur()
+                colonne = 0
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_2 or event.key == pygame.K_KP2):
-                position = get_lower_position(1)
-                if position:
-                    afficher_pion(play, 1, position)
-                    change_couleur()
+                colonne = 1
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_3 or event.key == pygame.K_KP3):
-                position = get_lower_position(2)
-                if position:
-                    afficher_pion(play, 2, position)
-                    change_couleur()
+                colonne = 2
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_4 or event.key == pygame.K_KP4):
-                position = get_lower_position(3)
-                if position:
-                    afficher_pion(play, 3, position)
-                    change_couleur()
+                colonne = 3
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_5 or event.key == pygame.K_KP5):
-                position = get_lower_position(4)
-                if position:
-                    afficher_pion(play, 4, position)
-                    change_couleur() 
+                colonne = 4
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_6 or event.key == pygame.K_KP6):
-                position = get_lower_position(5)
-                if position:
-                    afficher_pion(play, 5, position)
-                    change_couleur()
+                colonne = 5
+                position = get_lower_position(colonne)
             elif(event.key == pygame.K_7 or event.key == pygame.K_KP7):
-                position = get_lower_position(6)
-                if position:
-                    afficher_pion(play, 6, position)
-                    change_couleur()
+                colonne = 6
+                position = get_lower_position(colonne)
+
+            if position and colonne != -1:
+                afficher_pion(play, colonne, position)
+                change_couleur()
+        if event.type == pygame.MOUSEBUTTONUP and game:
+            posX = pygame.mouse.get_pos()[0]
+            colonne = -1
+            position = False
+            if posX > 4 and posX <= 95:
+                colonne = 0
+                position = get_lower_position(colonne)
+            elif posX > 95 and posX <= 192:
+                colonne = 1
+                position = get_lower_position(colonne)
+            elif posX > 192 and posX <= 288:
+                colonne = 2
+                position = get_lower_position(colonne)
+            elif posX > 288 and posX <= 384:
+                colonne = 3
+                position = get_lower_position(colonne)
+            elif posX > 384 and posX <= 480:
+                colonne = 4
+                position = get_lower_position(colonne)
+            elif posX > 480 and posX <= 576:
+                colonne = 5
+                position = get_lower_position(colonne)
+            elif posX > 576 and posX <= 672:
+                colonne = 6
+                position = get_lower_position(colonne)
+            if position and colonne != -1:
+                afficher_pion(play, colonne, position)
+                change_couleur()
 
     screen.blit(grille, (0, 0))
+
+    if pause:
+        borders_scale = 3
+        pygame.draw.rect(screen, (0, 0, 0), (WIDTH // 2 - 80 - 2 *borders_scale, 130 - borders_scale, 190 + 2*borders_scale, 260 + 2*borders_scale))
+        pygame.draw.rect(screen, (255, 255, 255), (WIDTH // 2 - 80 - borders_scale, 130, 190, 260))
+        if quit_button.draw(screen):
+            running = False
+        if options_button.draw(screen):
+            print("Options")
+
+        if resume_button.draw(screen):
+            pause = False
+            game = True
+            if play == ROUGE:
+                pygame.display.set_caption('Puissance 4 - Tour au Rouge')
+            elif play == JAUNE:
+                pygame.display.set_caption('Puissance 4 - Tour au Jaune')
 
     draw_pions()
     if check_win()[0]:
         if check_win()[1] == 'NUL':
             print("Match nul")
+            pygame.display.set_caption('Match nul')
         else:
             print(f"L'équipe {check_win()[1]} a gagné")
+            pygame.display.set_caption(f"Victoire de l'équipe {check_win()[1]}")
         game = False
-
- 
 
     pygame.display.flip()
     clock.tick(60)  
